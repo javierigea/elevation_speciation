d67fa9d8711c32a80c78131dbcc9321262434635
jigea <igea.javier@gmail.com>
2020-01-17 17:30:54 +0000

    1st commit -  backbone of analysis script

---
+++ analysis.R
@@ -0,0 +1,42 @@
+#this is the analysis script for the elevation_speciation repo
+
+#load libraries
+library(hexbin)
+library(spdep)
+library(ape)
+library(piecewiseSEM)
+
+####TO DO: delete local path here####
+setwd('~/Dropbox/Work_in_progress/elevation/')
+
+#raw_data folder contents
+#
+
+#folder structure etc
+dir.create('./plots/')
+dir.create('./output/')
+
+####1)phylogenetic data####
+#mammal phylogeny
+
+#bird phylogeny
+
+#taxonomy for mammals
+
+#taxonomy for birds
+
+####2)diversification rates####
+#DR
+
+#prepare BAMM input
+
+####3)species occurrence data, grid####
+
+####4)diversification measures in grid####
+
+####5)elevation and historic changes in elevation####
+
+####6)temperature and past temperature####
+
+
+

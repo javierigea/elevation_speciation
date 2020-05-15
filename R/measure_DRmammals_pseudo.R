ea590f81b9ce214d1a06d1a7edb905472ae7b528
jigea <igea.javier@gmail.com>
2020-05-15 17:26:15 +0100

    added cluster script to measure DR in mammal pseudoposterior

---
+++ R/measure_DRmammals_pseudo.R
@@ -0,0 +1,15 @@
+#run model in hydrogen cluster
+.libPaths('/home/ji247/R/x86_64-pc-linux-gnu-library/3.5')
+library(ape)
+library(ape)
+library(picante)
+
+source('./R/measure_DR.R')
+
+args<-commandArgs(trailingOnly = TRUE)
+print(args)
+replicate<-as.numeric(args[1])
+
+DRmammals_stats_grid_pseudoposterior_replicate(replicate = replicate)
+
+
